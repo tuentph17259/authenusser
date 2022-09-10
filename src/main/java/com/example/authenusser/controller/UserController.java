3f071f67b049a1a98506a2e57aab0da01f230473
tuentph17259 <tuentph17259@fpt.edu.vn>
2022-09-10 09:46:51 +0700

    save

--- src/main/java/com/example/authenusser/controller/UserController.java
+++ src/main/java/com/example/authenusser/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.authenusser.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+	@GetMapping("/user")
+	
 }
